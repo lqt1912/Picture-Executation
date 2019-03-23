b97b4019df8ab798201984cae72e3bb2ddefd528
Thang Le Quoc <32220420+lqt1912@users.noreply.github.com>
2019-03-23 15:20:39 +0700

    Commit

---
+++ Picture_Execute/Picture_Execute/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Picture_Execute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(196, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 290);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 76);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Split";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1652, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Matrix converted";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 76);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Remove Duplicate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 76);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Merge";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(834, 369);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Picture_Execute/Picture_Execute/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+namespace Picture_Execute
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // ;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        public static bool CompareBitmapsFast(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        static Bitmap[] arrBmp = new Bitmap[576];
+        int k = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(pictureBox1.Image);
+            int width = bitmap.Width;
+            int high = bitmap.Height;
+
+            for (int y = 0; y < bitmap.Height; y += 32)
+            {
+                for (int x = 0; x < bitmap.Width; x += 32)
+                {
+                    RectangleF cloneRect = new RectangleF(x, y, 32, 32);
+                    PixelFormat format = bitmap.PixelFormat;
+                    Bitmap cloneBitmap = bitmap.Clone(cloneRect, format);
+                    arrBmp[k++] = cloneBitmap;
+                }
+            }
+            Result1 rs1 = new Result1();
+
+            for (k = 0; k < (bitmap.Height*bitmap.Width)/(32*32); k++)
+            {
+                PictureBox picture = new PictureBox();
+                picture.Image = arrBmp[k];
+                picture.SizeMode = PictureBoxSizeMode.AutoSize;
+                rs1.flowLayoutPanel1.Controls.Add(picture);
+            }
+            rs1.Show();
+            //MessageBox.Show(rs1.flowLayoutPanel1.Controls.Count.ToString());
+        }
+        ArrayList arlPictureRef = new ArrayList();
+        private void button2_Click(object sender, EventArgs e)
+        {        
+          
+            bool isDuplicate = false;
+
+            foreach (Bitmap bmp in arrBmp)
+            {
+                Picture_refs pcr = new Picture_refs();
+                pcr.bmp_ref = bmp;
+                pcr.num_ref = 0;
+                for (int i = 0; i < arlPictureRef.Count; i++)
+                    if (CompareBitmapsFast((Bitmap)arlPictureRef[i], bmp) == true)
+                        isDuplicate = true;
+                if (isDuplicate == false)
+                    arlPictureRef.Add(bmp);
+                else
+                    isDuplicate = false;
+            }
+
+            Result rs = new Result();
+            for (int i = 0; i < arlPictureRef.Count; i++)
+            {
+
+                PictureBox picture = new PictureBox();
+                picture.Image = (Bitmap)arlPictureRef[i];
+                picture.SizeMode = PictureBoxSizeMode.AutoSize;
+                rs.flowLayoutPanel1.Controls.Add (picture);          
+            }
+
+            rs.Show();
+            //arlPictureRef: arrayList của các ảnh Bitmap đã rút gọn, kèm theo ID của nó 
+
+            
+            ArrayList arlPictureRef2 = new ArrayList();
+            foreach(Bitmap bmp in arrBmp)
+            {
+             
+                Picture_refs pcr = new Picture_refs();
+                pcr.bmp_ref = bmp;
+               
+                for ( int i=0; i<arlPictureRef.Count; i++)
+                {
+                    Bitmap nBmp = (Bitmap)arlPictureRef[i];
+                    int id = i;
+                    if (CompareBitmapsFast(bmp,nBmp) == true)
+                    {
+                        pcr.num_ref = id;
+                    }
+                                
+                } 
+                
+                arlPictureRef2.Add(pcr);
+            }
+
+            using (StreamWriter sw = new StreamWriter("matrixMatrix.txt"))
+            {
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                int c = 0;
+                sw.Write(arlPictureRef.Count + " " + bitmap.Height / 32 + " " + bitmap.Width / 32);
+                sw.WriteLine();
+
+                for (int i = 0; i < arlPictureRef2.Count; i++)
+                {
+                    Picture_refs pcr = (Picture_refs)arlPictureRef2[i];
+                    sw.Write(pcr.num_ref + " ");
+                    c++;
+                    if (c %(bitmap.Width/32) ==0)
+                        sw.WriteLine();
+                }           
+            }
+            //MessageBox.Show(rs.flowLayoutPanel1.Controls.Count.ToString());
+        }
+
+
+        public void CombineImage(ArrayList arl )
+        {
+           
+            int width = arl.Count*32;
+            int height = 32;
+            Bitmap newBitmap = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(newBitmap);
+            g.Clear(SystemColors.AppWorkspace);
+            int nIndex = 0;
+            foreach(object obj in arl)
+            {
+                Bitmap nBM = (Bitmap)obj;
+                if (nIndex == 0)
+                {
+                    g.DrawImage(nBM, new Point(0, 0));
+                    nIndex++;
+                    width = nBM.Width;
+                }
+                else
+                {
+                    g.DrawImage(nBM, new Point(width, 0));
+                    width += nBM.Width;
+                }
+                nBM.Dispose();
+            }
+
+            newBitmap.Save("myImage.bmp");
+            newBitmap.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                CombineImage(arlPictureRef);
+        }
+    }
+}
